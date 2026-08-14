bc47d32349512ade3713e3bf9e5623bdefebe181
slattthewizard <221410315+slattthewizard@users.noreply.github.com>
2026-08-14 14:12:09 +0300

    Update canonical site domain to texomatornadoshelters.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://texomastormshelters.com',
+  site: 'https://texomatornadoshelters.com',
   integrations: [sitemap()],
 });
